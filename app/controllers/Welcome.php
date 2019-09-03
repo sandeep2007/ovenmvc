9806e1b73ad39489b36f5a86d2ed195a72d70ca2
Sandeep Kumar <ki.sandeep11@gmail.com>
2019-09-03 14:48:00 +0530

    ...

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -10,20 +10,33 @@ class Welcome extends Controller
 
     public function index()
     {
-       
+
         $this->view->render('welcome');
-         //$app =& getInstance();
         
-         
+        $db = new DB();
+        //$users = $db->table('pr_merchant')->select(['company_name', 'email'])->where('id', '23')->limit(100)->get();
+
+        //$users = DB::select("select company_name from pr_merchant order by id DESC limit 1");
+
+        //$obj = new Test_model();
+
+
+        // echo '<pre>';
+        //print_r(Test_model::limit(2)->get());
+        
+        //print_r($obj->get());
+        //$app =& getInstance();
+
+
         // $app->model->table = 'pr_merchant';
-       //  $merchant = $app->model->get(['limit' => 50])->data();
+        //  $merchant = $app->model->get(['limit' => 50])->data();
 
         // $app->model->table = 'pr_countries';
         // $app->model->primary_key = 'country_id';
         // $banner = $app->model->get(['limit' => 3])->data();
         // echo '<pre>';
-       // print_r($merchant);
+        // print_r($merchant);
         // print_r($banner);
-        
+
     }
 }
